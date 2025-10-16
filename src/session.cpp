087007767844090bdfc6b4c325296bb082e046d6
Fredrik <fredrik.d.holmqvist@gmail.com>
2025-10-16 23:37:54 +0200

    sync auto-commit: #14

--- src/session.cpp
+++ src/session.cpp
@@ -26,10 +26,8 @@ Session::Session() {
 
     store_glsl_helper("assets/model.glsl");
 
-    // camera.pos = { 1.4, 1, 1.2 };
-    camera.pos = { 0.4, 0.15, 0.8 };
-    // camera.rot = { -140, -25 };
-    camera.rot = { -120, -6.5 };
+    camera.pos = { 2.5, 1.5, 2.5 };
+    camera.rot = { -135, -32 };
 
     // mesh = MeshStatic::from_scene("assets/room.obj", 0, 0, 0);
     mesh = MeshStatic::from_scene("assets/sofa.obj", 0, 0, 0);
@@ -100,11 +98,11 @@ Session::Session() {
             glBindTexture(GL_TEXTURE_2D, sector_t);
             glUniform1i(uloc("sector_t"), 0);
 
-            int n = 2;
+            int n = 4;
 
             for (int y = 0; y < n; y++) {
                 for (int x = 0; x < n; x++) {
-                    se.mesh.world_pos = { x, 0, y };
+                    se.mesh.world_pos = { (float)x / 2, 0, (float)y / 2 };
                     se.mesh.gl_uniforms(s.ID);
                     glDrawElements(GL_TRIANGLES, se.mesh.indices.size(), GL_UNSIGNED_SHORT, 0);
                 }
@@ -157,113 +155,3 @@ void Session::render() {
 
     window.swap();
 }
-
-/*
-
-geo = Shader(
-        "assets/flat_geo.vs", "assets/flat_geo.fs",
-        [this](Shader &) {
-            glGenFramebuffers(1, &buffer);
-            glBindFramebuffer(GL_FRAMEBUFFER, buffer);
-
-            // main color
-            glGenTextures(1, &color_t);
-            glBindTexture(GL_TEXTURE_2D, color_t);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCREEN_W, SCREEN_H, 0, GL_RGB, GL_FLOAT,
-                         nullptr);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color_t, 0);
-
-            // normals
-            glGenTextures(1, &normal_t);
-            glBindTexture(GL_TEXTURE_2D, normal_t);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCREEN_W, SCREEN_H, 0, GL_RGB, GL_FLOAT,
-                         nullptr);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, normal_t,
-                                   0);
-
-            // depth
-            glGenTextures(1, &depth_t);
-            glBindTexture(GL_TEXTURE_2D, depth_t);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, SCREEN_W, SCREEN_H, 0,
-                         GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_t, 0);
-
-            GLuint attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
-            glDrawBuffers(2, attachments);
-            assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
-
-            glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                                  (void*)(offsetof(Vertex, pos)));
-            glEnableVertexAttribArray(0);
-
-            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                                  (void*)(offsetof(Vertex, normal)));
-            glEnableVertexAttribArray(1);
-
-            mesh.gl_buffer_data();
-        },
-        [](Shader &s, Session &se) {
-            glUniformMatrix4fv(uloc("view"), 1, GL_FALSE, value_ptr(se.camera.view_matrix()));
-            glUniformMatrix4fv(uloc("projection"), 1, GL_FALSE, value_ptr(se.camera.perspective()));
-
-            glBindFramebuffer(GL_FRAMEBUFFER, buffer);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            const int n = 1;
-
-            for (int y = 0; y < n; y++) {
-                for (int x = 0; x < n; x++) {
-                    se.mesh.world_pos = { x, 0, y * 1.45 };
-                    se.mesh.gl_uniforms(s.ID);
-                    glDrawElements(GL_TRIANGLES, se.mesh.indices.size(), GL_UNSIGNED_SHORT, 0);
-                }
-            }
-
-            glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        });
-    result = Shader(
-        "assets/flat.vs", "assets/flat.fs",
-        [](Shader &) {
-            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);
-
-            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
-                                  (void*)(2 * sizeof(float)));
-            glEnableVertexAttribArray(1);
-
-            float quad_verts[] = { -1, 1, 0, 1, -1, -1, 0, 0, 1, -1, 1, 0,
-                                   -1, 1, 0, 1, 1,  -1, 1, 0, 1, 1,  1, 1 };
-
-            glBufferData(GL_ARRAY_BUFFER, sizeof(quad_verts), quad_verts, GL_STATIC_DRAW);
-        },
-        [](Shader &s, Session &se) {
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, depth_t);
-            glUniform1i(uloc("depth_t"), 0);
-
-            glActiveTexture(GL_TEXTURE1);
-            glBindTexture(GL_TEXTURE_2D, normal_t);
-            glUniform1i(uloc("normal_t"), 1);
-
-            glUniform2f(uloc("texel_size"), 1.0f / SCREEN_W, 1.0f / SCREEN_H);
-
-            glUniform2f(uloc("resolution"), SCREEN_W, SCREEN_H);
-
-            glUniformMatrix4fv(uloc("inv_proj"), 1, GL_FALSE,
-                               value_ptr(inverse(se.camera.perspective())));
-
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-        });
-
-*/
