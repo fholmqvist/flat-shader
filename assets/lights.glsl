b913ae36ab890d9c1581123a9be48e4c5451c7f3
Fredrik <fredrik.d.holmqvist@gmail.com>
2025-10-28 13:09:19 +0100

    REMOVED ASSETS

--- assets/lights.glsl
+++
@@ -1,6 +0,0 @@
-struct DirectionalLight {
-    float intensity;
-    vec3  dir;
-};
-
-uniform DirectionalLight dir_light;
--- assets/lights_calc.glsl
+++
@@ -1,5 +0,0 @@
-vec3 dir_light_calc(DirectionalLight l, vec3 normal, vec3 view_dir, vec3 diffuse) {
-    vec3 light_dir = normalize(-l.dir);
-    float diff = max(dot(normal, light_dir), 0);
-    return vec3(1) * diff * diffuse * l.intensity;
-}
